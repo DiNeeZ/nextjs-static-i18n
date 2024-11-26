98792f5682a9341f8e986e6b000a740b0d482bf1
DiNeeZ <dneez86@gmail.com>
2024-11-26 12:48:37 +0000

    first commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <div>
+      <h1>Hello Default Home</h1>
+    </div>
+  );
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,17 @@
+import { type Locale } from "@/i18n/routing";
+import { setRequestLocale, getTranslations } from "next-intl/server";
+
+type Props = {
+  params: { locale: Locale };
+};
+
+export default async function Home({ params }: Props) {
+  setRequestLocale((await params).locale);
+  const t = await getTranslations("HomePage");
+
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+    </div>
+  );
+}
